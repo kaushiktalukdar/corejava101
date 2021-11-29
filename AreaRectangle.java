3cf9c8619f18cf690630e0de3054e03309e1c3e9
kaushik_talukdar <kaushik.t@zaloni.com>
2021-11-29 20:44:53 +0530

    Final commit to find area of the Rectangle from QuadraticEquation Branch

--- AreaRectangle.java
+++ AreaRectangle.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 class AreaRectangle{
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the width of the rectangle: ");
+		// This variable will capture the width of the rectangle
+		float width = sc.nextFloat();
+		System.out.print("Enter the length of the rectangle: ");
+		// This variable will capture the length of the rectangle
+		float length = sc.nextFloat();
+		float Area=width*length;
+		System.out.printf("The area of the reactangle is: %.2f",Area);
 	}
 }
